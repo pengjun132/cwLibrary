6d8cff904de508a5dcb5e959f12ecea114322784
pengjun <1098325951@qq.com>
2019-02-16 17:03:47 +0800

    增加第一个测试类

--- demo.php
+++ demo.php
@@ -1,6 +1,9 @@
 <?php
 require 'vendor/autoload.php';
 use  pengjun132\cwlibrary\TestClass;
+use  pengjun132\cwlibrary\FirstClass;
 echo TestClass::test();
 echo "\n";
 echo TestClass::index();
+echo "\n";
+echo FirstClass::index();
---
+++ src/FirstClass.php
@@ -0,0 +1,10 @@
+<?php
+    namespace pengjun132\cwlibrary;
+
+    class FirstClass{
+        public static function index(){
+            return "this is my first class";
+        }
+    }
+
+?>
