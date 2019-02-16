3abc311169390d561b221c3439aefa1a9f9c9850
pengjun <1098325951@qq.com>
2019-02-16 15:08:37 +0800

    demo上传

---
+++ demo.php
@@ -0,0 +1,6 @@
+<?php
+require 'vendor/autoload.php';
+use  CW\cwlibrary\TestClass;
+echo TestClass::test();
+echo "\n";
+echo TestClass::index();
---
+++ src/TestClass.php
@@ -0,0 +1,15 @@
+<?php
+    namespace CW\cwlibrary;
+
+    class TestClass{
+        //测试方法
+        public static function test(){
+            return "test composer";
+        }
+
+        public static function index(){
+            return "this is my first test";
+        }
+    }
+
+?>
